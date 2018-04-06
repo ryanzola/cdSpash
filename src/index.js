eda010aefeeeec07bc29310dea0f176188c6c1ce
ryanzola <ryanzola@me.com>
2018-04-06 15:41:04 -0400

    using webpack and react to bundle and serve up all the things!

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import './styles.scss';
+
+class App extends Component {
+  render() {
+    return(
+      <div>
+        <div className="view"></div>
+        <div className="left">
+          <span className="text">welcome</span>
+          <span className="text">to</span>
+        </div>
+        <div className="right">
+          <span className="cd">chartDoc</span>
+        </div>
+        <div className="shadow-div"></div>
+        <div className="img-container">
+            <div className="comp"></div>
+        </div>
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById('react-container'))
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+module.exports = {
+  entry: path.join(__dirname, 'src', 'index'),
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['env', 'stage-0', 'react']
+        }
+      },
+      {
+        test: /\.css?$/,
+        use: [
+          {loader: 'style-loader'},
+          {loader: 'css-loader'}
+        ]
+      },
+      {
+        test: /\.scss?$/,
+        use: [
+          {loader: 'style-loader'},
+          {loader: 'css-loader'},
+          {loader: 'sass-loader'}
+        ]
+      },
+      {
+        test: /\.png?$/,
+        use: [
+          {loader: 'url-loader'}
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.json', '.js', '.jsx', '.css']
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    port: 5000
+  }
+};
