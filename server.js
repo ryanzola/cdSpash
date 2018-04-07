37c41a1b716d785bb17e3f9c2c750a1b725828bb
ryanzola <ryanzola@me.com>
2018-04-07 02:13:23 -0400

    env but ignored

--- server.js
+++ server.js
@@ -3,15 +3,13 @@ import path from 'path';
 
 const app = express();
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 5000);
 app.use(express.static('./dist'));
 app.use('/dist', express.static(__dirname + 'dist'));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 })
 
-app.listen(app.get('port'), () => {
-  console.info(`Server is listening on port ${app.get('port')}`)
-})
+app.listen(app.get('port'))
 
 module.exports = app;
