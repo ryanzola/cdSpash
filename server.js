14bb554c8dff76c4923df5bd691c68ba49f89166
ryanzola <ryanzola@me.com>
2018-04-07 11:52:35 -0400

    favicon and serve favicon middleware

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import path from 'path';
+import favicon from 'serve-favicon';
 
 const app = express();
 
 app.set('port', process.env.PORT || 5000);
 app.use(express.static('./dist'));
 app.use('/dist', express.static(__dirname + 'dist'));
+app.use(favicon(path.join(__dirname, 'favicon.ico')))
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 })
